f77f703a4f609d335eeb3b51b7a619d8890d65ef
l3miage-CHM <him.clemence@gmail.com>
2020-11-23 18:25:32 +0100

    Ajout fichier Connexion.java

---
+++ src/Modele/Connexion.java
@@ -0,0 +1,40 @@
+package Modele;
+
+import java.sql.*;
+
+public class Connexion {
+    private final static String URL = "jdbc:oracle:thin:@im2ag-oracle.e.ujf-grenoble.fr:1521:im2ag";
+    private final static String USER = "";
+    private final static String PASSWD = "";
+    private static Connection connect;
+
+    private Connexion() throws SQLException {
+        System.out.println("Tentative de Connexion au serveur");
+        try {
+            // Enregistrement du driver Oracle
+            System.out.print("Loading Oracle driver... ");
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());  	    System.out.println("loaded");
+
+            // Etablissement de la connection
+            System.out.print("Connecting to the database... ");
+            connect = DriverManager.getConnection(URL,USER,PASSWD);
+            System.out.println("connected");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connexion getInstance() throws SQLException {
+        return new Connexion();
+    }
+
+    public PreparedStatement prepareStatement(String requete) throws ClassNotFoundException, SQLException {
+        return connect.prepareStatement(requete);
+    }
+
+    public Statement createStatement() throws ClassNotFoundException, SQLException {
+        return connect.createStatement();
+    }
+
+}
